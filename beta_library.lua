fd79e6f1a2dfdeb459c82434cded59d385a86d44
Solibex <92078408+Solibex@users.noreply.github.com>
2025-08-11 01:14:38 +0700

    Update beta_library.lua

--- beta_library.lua
+++ beta_library.lua
@@ -217,6 +217,7 @@ do
 	end
 
 	local fonts = loadstring(game:HttpGet("https://fishy.solutions/assets/typeface.lua"))()
+	Library.fonts = fonts
 	local LibFont = fonts.load("minecraftia", 'https://fishy.solutions/assets/minecraftia.ttf')
 
 	Library.Font = LibFont
@@ -1747,7 +1748,6 @@ do
 
 			Library:Connect(user_input_service.InputBegan,  function(Input, gpe)
 				if gpe then return end
-				if Library:IsInventoryOpen() then return end
 
 				if Input.KeyCode == Keybind["Key"] and not Keybind.IsBeingSelected then
 					if Keybind["Mode"] == "Toggle" then
@@ -2693,7 +2693,6 @@ do
 		end);
 
 		Library:Connect(user_input_service.InputBegan,  function(Input, gpe)
-			if Library:IsInventoryOpen() then return end
 			if gpe then return end
 
 			if Input.KeyCode == Keybind["Key"] and not Keybind.IsBeingSelected then
